953e02cf564a62b3359a13f337763ad640488f7d
DaikiTsuzuki <dai62zk@gmail.com>
2024-07-21 15:26:46 +0900

    Update satellite structure parameters

--- src/src_user/Settings/SatelliteParameters/Sample/structure_parameters.c
+++ src/src_user/Settings/SatelliteParameters/Sample/structure_parameters.c
@@ -5,10 +5,10 @@
 
 #include <src_user/Settings/SatelliteParameters/structure_parameters.h>
 
-const float STRUCTURE_PARAMETERS_mass_sc_kg = 10.0;
+const float STRUCTURE_PARAMETERS_mass_sc_kg = 12.0;
 
-const float STRUCTURE_PARAMETERS_rmm_sc_body_Am2[PHYSICAL_CONST_THREE_DIM] = {0.3f, 0.2f, 0.1f};
+const float STRUCTURE_PARAMETERS_rmm_sc_body_Am2[PHYSICAL_CONST_THREE_DIM] = {0.05f, 0.05f, 0.05f};
 
-const float STRUCTURE_PARAMETERS_inertia_tensor_sc_body_kgm2[PHYSICAL_CONST_THREE_DIM][PHYSICAL_CONST_THREE_DIM] = {{0.1f, 0.0f, 0.0f},
-                                                                                                                    {0.0f, 0.2f, 0.0f},
-                                                                                                                    {0.0f, 0.0f, 0.3f} };
+const float STRUCTURE_PARAMETERS_inertia_tensor_sc_body_kgm2[PHYSICAL_CONST_THREE_DIM][PHYSICAL_CONST_THREE_DIM] = {{0.13f, 0.0f, 0.0f},
+                                                                                                                    {0.0f, 0.10f, 0.0f},
+                                                                                                                    {0.0f, 0.0f, 0.05f} };
