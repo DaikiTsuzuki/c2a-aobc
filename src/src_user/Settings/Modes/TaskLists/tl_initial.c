730e368cd97a6f1ead0c09e25af37a8b50623f73
DaikiTsuzuki <dai62zk@gmail.com>
2024-07-21 19:14:38 +0900

    ROUGH_THREE_AXIS_RW

--- src/src_user/Settings/Modes/TaskLists/tl_initial.c
+++ src/src_user/Settings/Modes/TaskLists/tl_initial.c
@@ -12,31 +12,34 @@
 
 void BCL_load_initial_mode(void)
 {
-  // CDH必須アプリ
-  BCL_tool_register_combine(0, BC_AC_CDH_UPDATE); // 23step以上
+	// CDH必須アプリ
+	BCL_tool_register_combine(0, BC_AC_CDH_UPDATE); // 23step以上
 
-  // AOCS必須アプリ
-  BCL_tool_register_combine(25, BC_AC_BASIC_SENSOR_UPDATE);  // 7step以上
-  BCL_tool_register_app    (35, AR_APP_AOCS_MM_INITIAL);     // 1step以上
+	// AOCS必須アプリ
+	BCL_tool_register_combine(25, BC_AC_BASIC_SENSOR_UPDATE);  // 7step以上
+	BCL_tool_register_app(35, AR_APP_AOCS_MM_ROUGH_THREE_AXIS); // 1step以上
 
-  // Initial mode用アプリ
-  BCL_tool_register_combine(36, BC_AC_RM3100_UPDATE);  // 4step以上
-  BCL_tool_register_combine(41, BC_AC_STIM210_UPDATE); // 1step以上
+	// 基本使わないがユーザーが使いたい時に使えるように入れておく
+	BCL_tool_register_combine(37, BC_AC_STIM210_UPDATE); // 1step以上
 
-  BCL_tool_register_combine(45, BC_AC_MTQ_UPDATE);     // 1step以上, MTQ駆動時間カウンタを20 [Hz]周期にするため入れている
+	// 粗三軸制御アプリ
+	BCL_tool_register_combine(40, BC_AC_MTQ_UPDATE);  // 1step以上, MTQ駆動時間カウンタを20 [Hz]周期にするため入れている
 
-  BCL_tool_register_combine(47, BC_AC_SUN_VECTOR_UPDATE);   // 合計4step以上必要ある？
-  BCL_tool_register_combine(51, BC_AC_INERTIAL_REF_UPDATE); // 11step以上
+	BCL_tool_register_combine(45, BC_AC_RM3100_UPDATE); // 4step以上
 
-  BCL_tool_register_combine(63, BC_AC_STT_UPDATE);     // 4step以上
-  BCL_tool_register_combine(78, BC_AC_OEM7600_UPDATE); // 1step以上
+	BCL_tool_register_combine(55, BC_AC_SUN_VECTOR_UPDATE); // 合計4step以上
+	BCL_tool_register_combine(60, BC_AC_INERTIAL_REF_UPDATE); // 合計11step以上
+	BCL_tool_register_app(72, AR_APP_ROUGH_THREE_AXIS_DETERMINATION); // 1step以上
 
-  BCL_tool_register_app    (80, AR_DI_RW0003);         // max 6msec
-  BCL_tool_register_app    (87, AR_APP_RW0003_FILTER); // 0msec
+	BCL_tool_register_app(75, AR_APP_THREE_AXIS_CONTROL_RW); // 1step以上
+	BCL_tool_register_app(78, AR_APP_UNLOADING); // 1step以上
 
-  // BCL_tool_register_app   (90, AR_APP_MODULE_TEST_BENCH);  // 打ち上げ時は必ずコメントアウトする
+	BCL_tool_register_combine(80, BC_AC_RW_UPDATE); // 10step以上
+	BCL_tool_register_combine(90, BC_AC_MTQ_UPDATE); // 1step以上
 
-  BCL_tool_register_combine(95, BC_AC_MTQ_UPDATE); // 1step以上
+	// 基本使わないがユーザーが使いたい時に使えるように入れておく
+	BCL_tool_register_combine(92, BC_AC_OEM7600_UPDATE); // 1step以上
+	BCL_tool_register_app(94, AR_DI_SAGITTA); // 4step以上
 }
 
 #pragma section
